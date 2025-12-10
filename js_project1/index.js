958a54d4126b947dd86ceb3bfbcc0a372095de81
Venkatesh Srigiri <104309202+venkateshsrigiri@users.noreply.github.com>
2025-12-10 15:47:32 +0530

    Add files via upload

---
+++ js_project1/index.js
@@ -0,0 +1,17 @@
+const body = document.getElementsByTagName("body")[0];
+
+function setColor(name){
+    body.style.color = name;
+}
+
+
+function randomColor(){
+    const red = Math.round(Math.random()*255);
+    const green =  Math.round(Math.random()*255);
+    const blue =  Math.round(Math.random()*255);
+
+    const color = `rgb(${red},${green},${blue})`
+    body.style.backgroundColor= color;
+
+}
+
